20c172b29b128e6b57eeae35cfa03059f22c2f58
Tomek Zawadzki <tomasz.zawadzki@swmansion.com>
2023-06-02 16:31:19 +0200

    Memoize ReanimatedModule in sendEvent

--- ios/REAEventDispatcher.m
+++ ios/REAEventDispatcher.m
@@ -1,4 +1,5 @@
 #import <RNReanimated/REAEventDispatcher.h>
+#import <RNReanimated/REAModule.h>
 #import <React/RCTBridge+Private.h>
 #import <React/RCTDefines.h>
 
@@ -6,7 +7,14 @@
 
 - (void)sendEvent:(id<RCTEvent>)event
 {
-  [[[self bridge] moduleForName:@"ReanimatedModule"] eventDispatcherWillDispatchEvent:event];
+  __weak static REAModule *reaModule;
+  __weak static RCTBridge *lastBridge;
+  RCTBridge *bridge = [self bridge];
+  if (bridge != lastBridge) {
+    reaModule = [bridge moduleForName:@"ReanimatedModule"];
+    lastBridge = bridge;
+  }
+  [reaModule eventDispatcherWillDispatchEvent:event];
   [super sendEvent:event];
 }
 
